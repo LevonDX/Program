1c94d8ab502f0d3048c2a5a875c12d772e1b6247
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-04-08 11:40:16 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Program
+{
+    class Program
+    {
+        private static Queue<int> stream = new Queue<int>();
+        private static Random random = new Random();
+        private static readonly Object o = new Object();
+
+        private static void Produce()
+        {
+            int input;
+
+            while (true)
+            {
+                input = random.Next(300, 1000);
+                Thread.Sleep(input);
+
+                lock (o)
+                {
+                    if (stream.Count > 10)
+                        Monitor.PulseAll(o);
+
+                    if (stream.Count > 100)
+                    {
+                        Monitor.PulseAll(o);
+                        Monitor.Wait(o);
+                    }
+
+                    stream.Enqueue(input);
+                    Console.WriteLine($"PRODUCE - {stream.Count}");
+                }
+            }
+        }
+
+        private static void Consume()
+        {
+            while (true)
+            {
+                int input = random.Next(300, 1000);
+                Thread.Sleep(input);
+
+                lock (o)
+                {
+                    if (stream.Count < 10)
+                    {
+                        Monitor.PulseAll(o);
+                        Monitor.Wait(o);
+                    }
+
+                    stream.Dequeue();
+                    Console.WriteLine($"CONSUME  - {stream.Count}");
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Thread t1 = new Thread(Produce);
+            t1.Name = "Producer";
+
+            Thread t2 = new Thread(Consume);
+            t2.Name = "Consumer";
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+        }
+    }
+}
